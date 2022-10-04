956f749bb0c335611f99e6ac383073ab14b1abc2
Marianne <mariannemongin@hotmail.fr>
2022-10-04 12:07:18 +0200

    update feature findbyid - try catch

--- src/main/java/com/example/marmitonwish/DetailsUserServlet.java
+++ src/main/java/com/example/marmitonwish/DetailsUserServlet.java
@@ -1,5 +1,6 @@
 package com.example.marmitonwish;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,11 +16,22 @@ public class DetailsUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String idStr = req.getParameter("id");
-        Optional<User> userOptional = DaoFactory.getUserDAO().findById(Long.parseLong(idStr));
 
-        if (userOptional.isPresent()) {
-            req.setAttribute("user", userOptional.get());
+        try {
+            long idUser = Long.parseLong(idStr);
+            Optional<User> userOptional = DaoFactory.getUserDAO().findById(idUser);
+
+            if (userOptional.isPresent()) {
+                req.setAttribute("user", userOptional.get());
+            }
+
+        } catch (NumberFormatException e) {
+            RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/removeUser.jsp");
+            req.setAttribute("error_format_id", true);
+            rd.forward(req, resp);
         }
+
+
 
         req.getRequestDispatcher("/WEB-INF/detailsUser.jsp").forward(req, resp);
     }
